486b8e07dc2185b6a0832ec6e5e8584f9c28cbdf
marwen <marwen.saidi@orange.com>
2025-04-10 13:35:41 +0200

    preparing task

--- src/test/java/io.github.systemmanagement/ComplexTest.java
+++ src/test/java/io.github.systemmanagement/ComplexTest.java
@@ -1,32 +1,12 @@
 package io.github.systemmanagement;
 
-import io.github.systemmanagement.order.DiscountService;
-import io.github.systemmanagement.order.Order;
-import io.github.systemmanagement.order.Product;
 import org.junit.jupiter.api.Test;
-import org.mockito.Spy;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 
 class ComplexTest {
 
-    @Spy
-    private Order order = new Order();
 
-    @Spy
-    private DiscountService discountService = new DiscountService();
 
     @Test
     void testComplexScenario() {
-        order.addProduct(new Product("Laptop", 100, 2)); // $200
-        order.addProduct(new Product("Mouse", 50, 2));   // $100
-
-        double discountedTotal = discountService.applyDiscount(order);
-
-        verify(order, times(1)).calculateTotal();
-        verify(discountService, times(1)).applyDiscount(order);
-
-        assertEquals(270, discountedTotal); // 10% discount on $300
     }
 }
--- src/test/java/io.github.systemmanagement/InvoiceGeneratorTest.java
+++ src/test/java/io.github.systemmanagement/InvoiceGeneratorTest.java
@@ -2,14 +2,9 @@ package io.github.systemmanagement;
 
 import io.github.systemmanagement.order.DiscountService;
 import io.github.systemmanagement.order.InvoiceGenerator;
-import io.github.systemmanagement.order.Order;
-import io.github.systemmanagement.order.Product;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Spy;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
 
 class InvoiceGeneratorTest {
 
@@ -21,17 +16,5 @@ class InvoiceGeneratorTest {
 
     @Test
     void testGenerateInvoice() {
-        Order order = new Order();
-        order.addProduct(new Product("Laptop", 120, 1));
-
-        when(discountService.applyDiscount(order)).thenReturn(108.0);
-
-        String invoice = invoiceGenerator.generateInvoice(order);
-
-        verify(invoiceGenerator, times(1)).generateInvoice(order);
-        verify(discountService, times(1)).applyDiscount(order);
-
-        assertTrue(invoice.contains("Laptop"));
-        assertTrue(invoice.contains("Total (after discount): $108.0"));
     }
 }
